a500e1d976058ad828e24371fff78680c12ecb7c
poetofcode <cyber-post@yandex.ru>
2024-04-05 18:44:50 +0300

    impl

--- app/worker/index.js
+++ app/worker/index.js
@@ -1,18 +1,40 @@
+const axios = require('axios');
+const { XMLParser } = require('fast-xml-parser');
+
+const parser = new XMLParser();
+
 async function launch(context) {
     console.log("Workers started");
 
-    setInterval(() => {
-        workers.forEach((worker) => worker(context));
-    }, 1000);
+    // setInterval(() => {
+    //     workers.forEach((worker) => worker(context));
+    // }, 5000);
+
+    workers.forEach((worker) => worker(context));
 }
 
+
 const jokeWorker = async (context) => { 
-    // console.log("Joke worker") 
+    const response = await axios({
+        method: 'get',
+        url: `http://rzhunemogu.ru/Rand.aspx`
+    });
+
+    let parsed = parser.parse(response.data);
+    const joke = parsed.root.content;
+    const jokeCollection = context.getDb().collection('jokes');
+
+    const newJoke = { text: joke };
+    const insertResult = await jokeCollection.insertOne(newJoke);
+
+    console.log(insertResult);
 }
+
 
 const workers = [
     jokeWorker
 ]
+
 
 
 exports.launch = launch;
