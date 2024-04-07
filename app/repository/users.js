844e3a08650ee6c6409b848b12011885384422bb
poetofcode <cyber-post@yandex.ru>
2024-04-07 19:23:23 +0300

    impl

--- app/repository/users.js
+++ app/repository/users.js
@@ -9,11 +9,17 @@ class UserRepository {
 	}
 
     async createUser(login, password) {
+		const foundUser = await this.userCollection.findOne({ login : login });
+        if (foundUser) {
+            return new Error(`User with login ${login} already exists`);
+        }
+
         const newUser = {
             createdAt: new Date(),
             login: login,
             password: utils.sha1(password),
         }
+
         const result = await this.userCollection.insertOne(newUser);
         return newUser;
     }
