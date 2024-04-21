8f3c9566ecaa66c3b115d2801feedaeba58027f4
poetofcode <cyber-post@yandex.ru>
2024-04-21 13:38:28 +0300

    impl

--- app/api/index.js
+++ app/api/index.js
@@ -16,28 +16,7 @@ async function initRoutes(router, context) {
 	const sessionMiddleware = middlewares.sessions;
 	const sessionRepository = repositories.sessions;
 
-	router.use(async function (req, res, next) {
-		const authHeader = req.header('Authorization');
-
-		// White-list эндпоинтов без авторизации:
-		const isSessionsPost = req.path === '/sessions' && req.method === 'POST';
-		const isSessionByTokenGet = req.path.startsWith("/sessions/") && req.path !== "/sessions/" && req.method === 'GET';
-		const isSessionByTokenDelete = req.path.startsWith("/sessions/") && req.path !== "/sessions/" && req.method === 'DELETE';
-		const isUserCreate = req.path.startsWith('/users') && req.method === 'POST';
-		if (isSessionsPost || isSessionByTokenGet || isSessionByTokenDelete || isUserCreate) {
-			return next();
-		}
-
-		if (authHeader) {
-			const token = authHeader.replace('Bearer ', '');
-            const session = await sessionRepository.fetchSessionByToken(token);
-            if (session) {
-            	return next();
-            }
-		}
-
-		res.status(401).send(utils.wrapError(new Error('Not authorized')));
-	});
+	router.use(handleTokenValidation);
 
 	router.post('/sessions', sessionMiddleware.createSession());
 	router.get('/sessions', sessionMiddleware.fetchSessions());
@@ -49,13 +28,40 @@ async function initRoutes(router, context) {
 	router.post('/subscriptions', middlewares.subscriptions.subscribe());
 	router.delete('/subscriptions', middlewares.subscriptions.unsubscribe());
 
-	router.use((err, req, res, next) => {
-	  if (res.headersSent) {
-    	return next(err)
-      }
-	  console.log(err);
-	  res.status(err.status || 500).send(utils.wrapError(err));
-	});
+	router.use(handleErrors);
 }
+
+async function handleTokenValidation(req, res, next) {
+	const authHeader = req.header('Authorization');
+
+	// White-list эндпоинтов без авторизации:
+	const isSessionsPost = req.path === '/sessions' && req.method === 'POST';
+	const isSessionByTokenGet = req.path.startsWith("/sessions/") && req.path !== "/sessions/" && req.method === 'GET';
+	const isSessionByTokenDelete = req.path.startsWith("/sessions/") && req.path !== "/sessions/" && req.method === 'DELETE';
+	const isUserCreate = req.path.startsWith('/users') && req.method === 'POST';
+	if (isSessionsPost || isSessionByTokenGet || isSessionByTokenDelete || isUserCreate) {
+		return next();
+	}
+
+	if (authHeader) {
+		const token = authHeader.replace('Bearer ', '');
+        const session = await sessionRepository.fetchSessionByToken(token);
+        if (session) {
+        	return next();
+        }
+	}
+
+	res.status(401).send(utils.wrapError(new Error('Not authorized')));
+}
+
+
+function handleErrors(err, req, res, next) {
+  if (res.headersSent) {
+	return next(err)
+  }
+  console.log(err);
+  res.status(err.status || 500).send(utils.wrapError(err));
+}
+
 
 exports.initRoutes = initRoutes;
