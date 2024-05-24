9762f8b6082ed7d8d7e7cb6a1812f0df164a5779
poetofcode <cyber-post@yandex.ru>
2024-05-24 23:41:25 +0300

    impl

--- app/utils/mailer.js
+++ app/utils/mailer.js
@@ -6,40 +6,40 @@ class Mailer {
     }
 
     async init() {
-		// let testEmailAccount = await nodemailer.createTestAccount();
-		// this.transporter = nodemailer.createTransport({
-		//     host: 'smtp.ethereal.email',
-		//     port: 587,
-		//     secure: false,
-		//     auth: {
-		//         user: testEmailAccount.user,
-		//         pass: testEmailAccount.pass,
-		//     },
-		// });
+		let testEmailAccount = await nodemailer.createTestAccount();
+		this.transporter = nodemailer.createTransport({
+		    host: 'smtp.ethereal.email',
+		    port: 587,
+		    secure: false,
+		    auth: {
+		        user: testEmailAccount.user,
+		        pass: testEmailAccount.pass,
+		    },
+		});
 
 		// this.transporter = nodemailer.createTransport({
 		//     service: 'gmail',
 		//     auth: {
-		//         user: 'user',
+		//         user: 'login',
 		//         pass: 'pass',
 		//     },
 		// });
 
-		this.transporter = await nodemailer.createTransport({
- 			service: 'Yandex',
-		    auth: {
-		      user: "example@mail.com", // generated ethereal user
-		      pass: "pass" // generated ethereal password
-		    }
-		});
+		// this.transporter = await nodemailer.createTransport({
+	 	// 	   service: 'Yandex',
+		//     auth: {
+		//         user: "login", // generated ethereal user
+		//         pass: "pass" // generated ethereal password
+		//     }
+		// });
     }
 
     async send(email, jokeText) {
-        // console.log(`[Mailer] email: ${email}, joke: ${jokeText}`);
+        console.log(`[Mailer] email: ${email}, joke: ${jokeText}`);
 
 		let result = await this.transporter.sendMail({
-		    from: '"Node js" <nodejs@example.com>',
-		    to: 'neverhood99@gmail.com',		 // ', user@example.com',
+		    from: '"Node js" <example@gmail.com>',
+		    to: 'example@gmail.com',
 		    subject: 'Message from Node js',
 		    text: 'This message was sent from Node js server.',
 		    html:
