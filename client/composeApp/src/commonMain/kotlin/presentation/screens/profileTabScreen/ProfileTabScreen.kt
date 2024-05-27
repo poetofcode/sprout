cb65d899b211c2eda23c680f272436aad88a4565
poetofcode <cyber-post@yandex.ru>
2024-05-27 14:10:01 +0300

    SproutClient: impl

--- client/composeApp/src/commonMain/kotlin/presentation/screens/profileTabScreen/ProfileTabScreen.kt
+++ client/composeApp/src/commonMain/kotlin/presentation/screens/profileTabScreen/ProfileTabScreen.kt
@@ -2,12 +2,19 @@ package presentation.screens.profileTabScreen
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.multiplatform.webview.web.rememberWebViewNavigator
 import presentation.Tabs
 import presentation.navigation.BaseScreen
@@ -37,6 +44,8 @@ class ProfileTabScreen : BaseScreen<ProfileTabViewModel>() {
 
         BackHandler { onBackClick() }
 
+        val isAuth = false
+
         MaterialTheme {
             Column {
                 TopAppBar(
@@ -57,9 +66,47 @@ class ProfileTabScreen : BaseScreen<ProfileTabViewModel>() {
                 )
 
                 Box(modifier = Modifier.fillMaxWidth().weight(1f)) {
-                    // TODO
+                    if (!isAuth) {
+                        UnsignedProfile()
+                    } else {
+                        SignedProfile()
+                    }
                 }
 
+            }
+        }
+    }
+
+    @Composable
+    fun UnsignedProfile() {
+        Box(Modifier.fillMaxSize()) {
+            Column(Modifier.wrapContentSize().align(Alignment.Center)) {
+                Text(
+                    text = "Войдите в аккаунт, чтобы получить доступ ко всем функциям приложения",
+                    color = Color.Gray,
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+                Button(
+                    modifier = Modifier.align(Alignment.CenterHorizontally).padding(vertical = 30.dp),
+                    onClick = {
+
+                    }
+                ) {
+                    Text(text = "Войти в аккаунт")
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun SignedProfile() {
+        Box(Modifier.fillMaxSize()) {
+            Column(Modifier.wrapContentSize().align(Alignment.Center)) {
+                Text(
+                    text = "Вы уже авторизованы",
+                    color = Color.Gray,
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
             }
         }
     }
