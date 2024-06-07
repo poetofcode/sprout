cfc0614566b8eeca4b2a63b99f6ea8dd466a4bd1
poetofcode <cyber-post@yandex.ru>
2024-06-07 18:34:47 +0300

    SproutClient: impl

--- client/composeApp/src/commonMain/kotlin/presentation/navigation/BaseScreen.kt
+++ client/composeApp/src/commonMain/kotlin/presentation/navigation/BaseScreen.kt
@@ -8,9 +8,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -24,8 +21,6 @@ import presentation.base.ViewModelStore
 import presentation.base.collectEffects
 
 
-typealias AnyScreen = BaseScreen<*>
-
 interface Screen<T : ViewModel<*>> {
     val screenId: String
 
@@ -34,6 +29,7 @@ interface Screen<T : ViewModel<*>> {
     @Composable
     fun Content()
 }
+
 
 abstract class BaseScreen<T : BaseViewModel<*>> : Screen<T> {
 
@@ -97,64 +93,4 @@ abstract class BaseScreen<T : BaseViewModel<*>> : Screen<T> {
         }
     }
 
-}
-
-interface NavState {
-    val screens: State<List<AnyScreen>>
-
-    fun push(screen: AnyScreen)
-
-    fun pop()
-
-    fun moveToFront(screenId: String)
-}
-
-class NavStateImpl(val viewModelStore: ViewModelStore) : NavState {
-    private val _screens = mutableStateOf<List<AnyScreen>>(emptyList())
-
-    override val screens: State<List<AnyScreen>>
-        get() = _screens
-
-    override fun push(screen: AnyScreen) {
-        screen.setVMStore(viewModelStore)
-        _screens.value += screen
-    }
-
-    override fun pop() {
-        if (_screens.value.size <= 1) {
-            return
-        }
-        _screens.value.lastOrNull()?.let { topScreen ->
-            viewModelStore.removeViewModel(topScreen.viewModel)
-        }
-        _screens.value = _screens.value.subList(0, _screens.value.size - 1)
-    }
-
-    override fun moveToFront(screenId: String) {
-        var currScreens = _screens.value.toMutableList()
-        currScreens.firstOrNull {
-            it.screenId == screenId
-        }?.let { found ->
-            currScreens = currScreens.filterNot { it.screenId == screenId }.toMutableList()
-            currScreens.add(found)
-            _screens.value = currScreens
-        }
-    }
-
-}
-
-
-@Composable
-fun Navigator(
-    modifier: Modifier = Modifier,
-    state: NavState,
-    tag: NavigatorTag = NavigatorTag.NONE,
-) {
-    val navigators = LocalNavigators.current + NavigatorInfo(state, tag)
-    CompositionLocalProvider(LocalNavigators provides navigators) {
-        Box(modifier) {
-            val screens = state.screens.value
-            screens.lastOrNull()?.PrepareContent()
-        }
-    }
 }
