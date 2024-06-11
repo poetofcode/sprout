9fb579916ca5adb082cb9b53056e5dfddd1174a3
poetofcode <cyber-post@yandex.ru>
2024-06-11 23:34:52 +0300

    impl

---
+++ app/repository/jokes.js
@@ -0,0 +1,19 @@
+const ObjectId = require("mongodb").ObjectId;
+const { utils } = require('../utils');
+
+class JokesRepository {
+
+	constructor(context) {
+		this.context = context;
+		this.db = context.getDb();
+		this.jokesCollection = this.db.collection('jokes');
+	}
+
+    async fetchJokes(login) {
+        const jokes = await this.jokesCollection.find({}).sort({ _id: -1 }).limit(20).toArray();
+        return jokes;
+    }
+
+}
+
+exports.create = (context) => new JokesRepository(context); 
