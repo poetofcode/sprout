93205878ecb3de43a37873258e9acd3a5cbb5f17
poetofcode <cyber-post@yandex.ru>
2024-07-14 21:29:21 +0300

    SproutClient: impl

--- client/composeApp/src/androidMain/kotlin/com/poetofcode/sproutclient/messaging.kt
+++ client/composeApp/src/androidMain/kotlin/com/poetofcode/sproutclient/messaging.kt
@@ -1,7 +1,6 @@
 package com.poetofcode.sproutclient
 
 import android.content.Context
-import android.util.Log
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.messaging.FirebaseMessaging
 
@@ -11,14 +10,10 @@ import com.google.firebase.messaging.FirebaseMessaging
     https://medium.com/@ravisharma23523/sending-notifications-to-mobile-devices-with-firebase-cloud-messaging-fcm-in-node-js-8fe3faead58b
  */
 
-// TODO Remove:
-//      esFT4_iUTxmToOG5v3hgND:APA91bGIOAToVV1nA9_sq6VZXEpOBZQBUrAEeC1P_HwIZIXXzLBlXlZJ7EY0y6pzgS_VDiYWPesXRWp2XRw-JYflVW-8oJ5xyvogtX6B4c8z9QPJoui1Abws0xYIbLw0bQs34MqNYuU6
-//      POCO: dMFX3x94RmOngId8Twbk2J:APA91bHIjMYfo0M1DmPru-Eh-geUwS1PsBsznEpHYIFVPYT_6uu5oa_PP9jN-u6pEuR8Eur4SAZgzudaJvyoV5amVDO2IIzOykTrySZYuttn2jplUKZ-egFr2fnjiHvtt1frDH55gLO1
-
 fun Context.retrieveFirebasePushToken(onSuccess: (String) -> Unit) {
     FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
         if (!task.isSuccessful) {
-            Log.w("mylog", "Fetching FCM registration token failed", task.exception)
+            task.exception.printStackTrace()
             return@OnCompleteListener
         }
 
