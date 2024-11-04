2cb23b296ab2f2d9f6eaebd750850ee719a55d3c
poetofcode <cyber-post@yandex.ru>
2024-11-04 16:03:50 +0300

    'impl'

--- init_script/index.js
+++ init_script/index.js
@@ -1,10 +1,25 @@
-// const app = require('./app').app;
-// var fs = require('fs');
+const jetpack = require("fs-jetpack");
 
 (async () => {
 	try {
 		console.log("Starting...");
-		
+
+		const src = jetpack.cwd("../app");
+		// const dst = jetpack.cwd("path/to/destination");
+
+		src.find({ matching: "*" }).forEach((path) => {
+		  const content = src.read(path);
+		  // const transformedContent = transformTheFileHoweverYouWant(content);
+
+		  console.log(`Walking path: ${path}`);
+
+		  if (path.includes('mailer.js')) {
+		  	console.log(`File contents:\n\n${content}\n\n`);
+		  }
+
+		  // dst.write(path, transformedContent);
+		});
+
 	} catch(err) {
 	    return console.log(err);
 	} 
