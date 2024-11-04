4ff8775e714ed75b340be448435b04c7c5396a5a
poetofcode <cyber-post@yandex.ru>
2024-11-04 15:47:32 +0300

    impl

---
+++ init_script/index.js
@@ -0,0 +1,11 @@
+// const app = require('./app').app;
+// var fs = require('fs');
+
+(async () => {
+	try {
+		console.log("Starting...");
+		
+	} catch(err) {
+	    return console.log(err);
+	} 
+})();
