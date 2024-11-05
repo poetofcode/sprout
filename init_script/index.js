deac7d30b37d0d3f63a26d1aa92477c0babcdea1
poetofcode <cyber-post@yandex.ru>
2024-11-05 06:38:11 +0300

    impl

--- init_script/index.js
+++ init_script/index.js
@@ -1,5 +1,25 @@
 const jetpack = require("fs-jetpack");
 
+class Replacer {
+
+	constructor(source, replaced) {
+		this.source = source;
+		this.replaced = replaced;
+	}
+
+	process(path, content, next) {
+		if (!content.includes(this.source)) {
+			next(path, content);
+			return;
+		}
+		console.log(`Replacer working on "${path}", replaced string "${this.source}"`);
+		const newContent = content.replaceAll(this.source, this.replaced);
+		next(path, newContent);
+	}
+
+}
+
+
 (async () => {
 	try {
 		console.log("Starting...");
@@ -7,18 +27,16 @@ const jetpack = require("fs-jetpack");
 		const src = jetpack.cwd("../app");
 		const dst = jetpack.cwd("../../scaffold");
 
+		const logReplacer = new Replacer('console.log("', 'console.log("Prefixed by Replacer: ');
+
 		src.find({ matching: "*" }).forEach((path) => {
 		  const content = src.read(path);
-		  // const transformedContent = transformTheFileHoweverYouWant(content);
-		  const transformedContent = content;
 
 		  console.log(`Walking path: ${path}`);
 
-		  if (path.includes('mailer.js')) {
-		  	console.log(`File contents:\n\n${content}\n\n`);
-		  }
-
-		  dst.write(`app/${path}`, transformedContent);
+		  logReplacer.process(path, content, (newPath, newContent) => {
+			  dst.write(`app/${newPath}`, newContent);
+		  });
 		});
 
 	} catch(err) {
