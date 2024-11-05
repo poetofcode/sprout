7041496134da424cedc7235996ea4c86fe4ce429
poetofcode <cyber-post@yandex.ru>
2024-11-05 06:58:01 +0300

    impl

--- init_script/index.js
+++ init_script/index.js
@@ -19,6 +19,18 @@ class Replacer {
 
 }
 
+class Copier {
+
+	constructor() {
+
+	}
+
+	process(path, content, next) {
+		
+	}
+
+}
+
 
 (async () => {
 	try {
@@ -28,15 +40,27 @@ class Replacer {
 		const dst = jetpack.cwd("../../scaffold");
 
 		const logReplacer = new Replacer('console.log("', 'console.log("Prefixed by Replacer: ');
+		const secondReplacer = new Replacer('Prefixed by Replacer: ', 'Prefixed by Replacer - ');
+
+		const copier = (newPath, newContent) => {
+			dst.write(`app/${newPath}`, newContent);
+	  	}
+
+	  	const handlers = [logReplacer, secondReplacer];
+
 
 		src.find({ matching: "*" }).forEach((path) => {
 		  const content = src.read(path);
 
 		  console.log(`Walking path: ${path}`);
 
-		  logReplacer.process(path, content, (newPath, newContent) => {
-			  dst.write(`app/${newPath}`, newContent);
-		  });
+		  const secondHandler = (p, c) => {
+		  		secondReplacer.process(p, c, copier);
+		  };
+		  logReplacer.process(path, content, secondHandler);
+
+		  // logReplacer.process(path, content, copier);
+
 		});
 
 	} catch(err) {
